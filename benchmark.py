0436c5b80f6c090263e98648e6dd5275b6f937a7
Your Name <you@example.com>
2020-01-21 10:32:27 +0100

    Add cosse

--- benchmark.py
+++ benchmark.py
@@ -1,11 +1,15 @@
+from random import randrange
+from timeit import time
+from selection_sort import selection_sort
+
 #Creo la lista
-#........
+mylist = [randrange(1000) for _ in range(10)]
 
 
 
 #Calcolo i tempi del selection sort....
-#....
-
+selection_sort_time = selection_sort(mylist.copy())
+    return stop_time - start_time
 #Calcolo i tempi del bubble sort...
 #....
 
@@ -15,6 +19,5 @@
 
 
 
-
 #Salvo i risultati del file...
-#...
+
